f6408f504dd7e8efaf34221f2d9badbe7f93d0f6
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-04-16 11:30:44 -0800

    WIP researching functions

---
+++ src/js/space-calc.js
@@ -0,0 +1,15 @@
+export default class Age {
+  constructor(earth, mercury, venus, mars, jupiter) {
+    this.earth = "";
+    this.mercury = "";
+    this.venus = "";
+    this.mars = "";
+    this.jupiter = "";
+  }
+}
+
+checkType();{
+  let earthAge = ""
+  function earthAge()
+
+}
