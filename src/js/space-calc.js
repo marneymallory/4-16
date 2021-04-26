0af1f103d5a90b3fd34ee1ed554ab73e18545681
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-04-25 16:37:07 -0800

    add good fail for 'past life expectancy' test

--- src/js/space-calc.js
+++ src/js/space-calc.js
@@ -1,39 +1,36 @@
 export default class Age {
   constructor(earthAge) {
-    this.earth = earthAge
-    this.earthAgeExpectancy = 78
+    this.earth = earthAge;
+    this.earthAgeExpectancy = 78;
   }
-  earthAge(){
-    return this.earth
+  earthAge() {
+    return this.earth;
   }
-  mercuryAge(){
-    return this.earth /.24
+  mercuryAge() {
+    return this.earth / 0.24;
   }
-  venusAge(){
-    return this.earth /.62
+  venusAge() {
+    return this.earth / 0.62;
   }
-  marsAge(){
-    return this.earth /1.88
+  marsAge() {
+    return this.earth / 1.88;
   }
-  jupiterAge(){
-    return this.earth /11.86
+  jupiterAge() {
+    return this.earth / 11.86;
   }
-  timeOnEarth(){
-    return this.earthAgeExpectancy - this.earth
+  timeOnEarth() {
+    return this.earthAgeExpectancy - this.earth;
   }
-  timeOnMercury(){
-    return (this.earthAgeExpectancy - this.earth) /.24
+  timeOnMercury() {
+    return (this.earthAgeExpectancy - this.earth) / 0.24;
   }
-  timeOnVenus(){
-    return (this.earthAgeExpectancy - this.earth) /.62
+  timeOnVenus() {
+    return (this.earthAgeExpectancy - this.earth) / 0.62;
   }
-  timeOnMars(){
-    return (this.earthAgeExpectancy - this.earth) /1.88  
+  timeOnMars() {
+    return (this.earthAgeExpectancy - this.earth) / 1.88;
   }
-  timeOnJupiter(){
-    return (this.earthAgeExpectancy - this.earth) /11.86
+  timeOnJupiter() {
+    return (this.earthAgeExpectancy - this.earth) / 11.86;
   }
-
-  }
-
-
+}
